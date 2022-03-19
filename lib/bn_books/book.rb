7f4c8634522e62356fa0d3abb26d6b2366559d51
Keenah <kvancamp@mail.com>
2022-03-19 00:10:56 -0500

    Started book class

--- lib/bn_books/book.rb
+++ lib/bn_books/book.rb
@@ -1,6 +1,22 @@
 class BnBooks::Book
+    @@all = []
+    attr_accessor :title, :author, :url, :price
 
+    def initialize
+        @title = title
+        @author = author
+        @url = url
+        @price = price
+       save
 
+    end
 
+    def self.all
+        BnBooks::Scraper.scrape_books if @@all.empty?
+        @@all
+    end
 
+    def save
+        @all << self
+    end
 end
