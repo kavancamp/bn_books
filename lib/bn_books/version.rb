651a8708a8f02cb9e6b7e8f1bef187c31ed292b3
Keenah <kvancamp@mail.com>
2022-03-18 23:07:01 -0500

    created gem project

---
+++ lib/bn_books/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BnBooks
+  VERSION = "0.1.0"
+end
---
+++ spec/bn_books_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BnBooks do
+  it "has a version number" do
+    expect(BnBooks::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
