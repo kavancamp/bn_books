60c205295db42beda81fb19956cf08725b10a1e3
Keenah <kvancamp@mail.com>
2022-03-18 23:56:50 -0500

    started to build menu

--- lib/bn_books/cli.rb
+++ lib/bn_books/cli.rb
@@ -5,9 +5,17 @@ class BnBooks::CLI
         puts "  Welcome To FreeBooks!!! "
         puts "        ***** "
 
-        
+        #categories
+        #menu
     end
 
-   
+    def menu
+        while input != "exit"
+        Please "/nEnter a number, 'list' to display categories, or 'exit'"
+
+        input = gets.strip
+        if (input.to_i > 0) && (input.to_i < 10)
+            
+    end
 
 end
