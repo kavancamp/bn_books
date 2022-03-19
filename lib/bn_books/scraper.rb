bd3c067ffed2fadf88b72bac40e4158e61e5bb03
Keenah <kvancamp@mail.com>
2022-03-19 16:45:54 -0500

    defined category

--- lib/bn_books/scraper.rb
+++ lib/bn_books/scraper.rb
@@ -1,7 +1,18 @@
 class BnBooks::Scraper 
 
 @@all = []
+def category
+    @@all.delete_if {|cat| (cat.include? " ") || (cat.include? "/") || (cat.include? "Postmodernism") || (cat.include? "Religion") || (cat.include? "Technology") || (cat.include? "Environment") || (cat.include? "Communications")}
+end
+def scrape_categories
+    doc = Nokogiri::HTML(open("https://www.barnesandnoble.com/h/books/browse"))
+    category = doc.css("div.html-embed-container")
+    category.each do |name|
+      @@all << name.css("h3").text
+    end
+    @@all.shift
 
+  end
 
 
 end
