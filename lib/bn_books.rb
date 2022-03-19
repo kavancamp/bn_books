2250a0f104a438579d4d321a783b13c450073b39
Keenah <kvancamp@mail.com>
2022-03-18 23:10:29 -0500

    renamed file

--- lib/bn_books.rb
+++ lib/bn_books.rb
@@ -1,6 +1,10 @@
-# frozen_string_literal: true
-
 require_relative "bn_books/version"
+require_relative "bn_books/cli"
+require_relative "bn_books/scraper"
+require_relative "bn_books/book"
+
+require 'pry'
+require 'nokogiri'
 
 module BnBooks
   class Error < StandardError; end
