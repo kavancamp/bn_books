98aa97991deda80a2b5d7efc3ec8422fecee75d9
Keenah <kvancamp@mail.com>
2022-03-19 01:25:32 -0500

    built out display

--- lib/bn_books/book.rb
+++ lib/bn_books/book.rb
@@ -1,12 +1,12 @@
 class BnBooks::Book
     @@all = []
-    attr_accessor :title, :author, :url, :price
+    attr_accessor :title, :author, :url, 
 
     def initialize
         @title = title
         @author = author
         @url = url
-        @price = price
+    
        save
 
     end
@@ -18,5 +18,14 @@ class BnBooks::Book
 
     def save
         @all << self
+    end
+
+    def self.display_books
+        puts "Books in this category :"
+        puts ""
+        Book.all.each_with_index do |book,index|
+          if book.title
+            puts "#{index + 1}- Title: #{book.title} Author: #{book.author} Url: #{book.url}"
+        end
     end
 end
