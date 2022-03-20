c0f2eb14dc2efefcaa59e46068c49aa13bdf9356
Keenah <kvancamp@mail.com>
2022-03-19 23:51:01 -0500

    Update cli.rb

--- lib/bn_books/cli.rb
+++ lib/bn_books/cli.rb
@@ -1,65 +1,76 @@
 class BnBooks::CLI 
-
-    def call 
-        greeting
-        categories
+    def call
+        puts "                         Welcome To BNBooks CLI!!! "
+        category_list
+        print_books
         menu
-    end
-
-    def greeting
-        puts "  ** Welcome To BN Books CLI **"
-    end
-
-    def categories
-        cat = BnBooks::Scraper.new
-        cat.scrape_categories
-        cat.category.each_with_index(1) do |category, index|
-            puts "#{index} #{category}"
-        category(input) if is_valid?(input, @cat)
-        end
-    end
-
-    def is_valid?(input, data) #check validity of input
-        input.to_i <= data.length && input.to_i > 10
-    end
-
-    def menu
+       
+      end
     
+      def category_list
+        x = Scraper.new
+        x.scrape_category
+        x.category.each.with_index(1) do |index, category|
+        puts "\n#{category}. #{index}"
+        end
+      end
+    
+      def menu
         input = nil
         while input != "exit"
-        puts "/nEnter a number, 'list' to display categories, or 'exit' "
+        puts "\nPlease type Book # or type exit!"
         input = gets.strip.downcase
-          #Capture input using gets
-            #.strip method to remove any new lines or leading and trailing whitespace
-        if input.to_i > 0 && input.to_1 < 3
-            cat.scrape_categories
-            cat.scrape_book(cat.category[input.to_i-1])
-                
-                Book.display_books
-                book_choice
-            elsif input == 'exit'
-                puts 'goodbye'
-            elsif input == 'list'
-                list_events
-            else 
-            puts "\n Invalid Entry, please enter 'list', 'exit', or enter the number of a book for more information on"
-            end
-        end
-    end
-    
-    def book_choice
-   
-        input = nil
-        while input != "exit"
-        puts "Select a book number for further information, 'list' to get categories or 'exit' !"
-        input = gets.strip.downcase
-        if (input.to_i > 0) && (input.to_i < 10)
-          system("open /https://www.barnesandnoble.com/h/books/browse#{Book.all[input.to_i-1].url}")
+        if (input.to_i > 0) && (input.to_i <= 20)
+          Book.clear
+          Scraper.clear
+          x = Scraper.new
+          x.scrape_category
+          x.scrape_book(x.category[input.to_i-1])
+          Book.print_books
+          choose_book
+          break
+        elsif input == "list"
+          Scraper.clear
+          category_list
         else
-            input == "list"
-              categories
-              menu
-         end
-       end
+    
+          end
+        end
+      end
+    
+      def print_books
+        puts "Here is your selected book:"
+        puts ""
+      input =  BnBooks::Book.find_by_index(input.to_i - 1)
+          if book.title
+    
+            puts "#{index}- Title: #{book.title}"
+    
+            puts "    Author: #{book.author}"
+    
+            puts "     URL : #{book.url}"
+            puts "------------------------"
+          end
+        end
+      end
+
+      def choose_book
+         input = nil
+         while input != "exit"
+         puts "Select a book number to go to the web page or list to get categories or exit !"
+         input = gets.strip
+         if (input.to_i > 0) && (input.to_i < 20)
+           system("open https://www.barnesandnoble.com/w/#{Book.all[input.to_i-1].url}")
+           elsif
+             input == "list"
+               Scraper.clear
+               category_list
+               menu
+               break
+             else
+               break
+            end
+          end
+        end
+
     end
-end
