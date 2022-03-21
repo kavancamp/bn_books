3b52a3b01b8b94d62447ed5d511c8a6762c59810
Keenah <kvancamp@mail.com>
2022-03-20 23:59:41 -0500

    Update book.rb

--- lib/bn_books/book.rb
+++ lib/bn_books/book.rb
@@ -1,32 +1,42 @@
 class Book
+    attr_accessor :title, :author, :url, :price
 
-attr_accessor :title, :author, :url, :price
-  @@all = []
+    @@all = []
+  
+    def initialize(title, author, url, date)
+      @title = title
+      @aauthor = author
+      @url = url
+      @price = price
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def self.find_by_index(index)
+      @@all[index]
+    end
 
-  def initialize
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
   def self.clear
     @@all =[]
   end
- 
-  def self.display_book_details
-    puts "Books in this category :"
-    puts ""
-        Book.all.each_with_index do |book,index|
-        if book.title
 
+  def self.display_book_details
+    puts "More info on selected title :"
+    puts ""
+      Book.all.each_with_index do |book,index|
+          if title.title
             puts "#{index + 1}- Title: #{book.title}"
-            puts "    Author: #{book.author}"
-            puts "     URL : #{book.url}"
+            puts " Author: #{book.author}"
+            puts " URL : #{book.url}"
+            puts " Price: #{book.price}"
             puts "------------------------"
+          end
         end
-        end 
-    end 
-  
+      end
+
+
 
 end
