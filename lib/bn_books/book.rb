2a4d44a866c6d4dad088f04b071f61e096a9058c
Keenah <kvancamp@mail.com>
2022-03-21 15:50:02 -0500

    Added notes

--- lib/bn_books/book.rb
+++ lib/bn_books/book.rb
@@ -1,18 +1,22 @@
-class BnBooks::Book
+class BnBooks::Book 
     attr_accessor :title, :books, :author, :price, :url
-
+   #read and write accessors
     @@all = []
+
+    #A variable prefixed with @ is an instance variable
+    #@@ is a Class variable or static variable it can be inherited. 
+    #This means that if you create a subclass of that class, it will inherit the variable
   
-    def initialize(title, author, price, url)
-      @title = title
+    def initialize(title, author, price, url) #set values for objects
+      @title = title    #instance variables @
       @author = author
       @price = price
       @url = url
-    
+     
       save
     end
   
-    def self.all
+    def self.all #class method for reading data in variable @@all
       BnBooks::Scraper.scrape_best_sellers if @@all.empty?
       @@all
     end
