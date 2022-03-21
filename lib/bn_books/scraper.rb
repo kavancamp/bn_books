5ed338a0e4920b2322b7e7a0cf38df991f9e5c61
Keenah <kvancamp@mail.com>
2022-03-21 14:07:32 -0500

    combined scrapers

--- lib/bn_books/scraper.rb
+++ lib/bn_books/scraper.rb
@@ -1,38 +1,21 @@
-class Scraper
+class BnBooks::Scraper
 
-  @@all = []
-
-  def best
-    @@all
-  end
-
-  def scrape_best_sellers
-    doc = Nokogiri::HTML(open("https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8"))
-    best = doc.css("div.product-info-view")
-    best.each do  |name|
-      @@all << name.css("h3 a").text.strip
-    end
-   
-  end
-
-
-  def scrape_book(input)
-    doc = Nokogiri::HTML(open("https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8"))
+  def self.scrape_best_sellers
+    page = Nokogiri::HTML(open("https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8"))
+    books = page.css("div.product-info-view")
+    books.each do  |data|
+      title = data.css("h3 a").text.strip.chomp
+      author = data.css("div.product-shelf-author").text.strip
+      price = data.css("a.current.link").text.strip
+      url = data.css("a").attr("href").text
+      BnBooks::Book.new(title, author, price, url)
       
-    book = doc.css("div.product-info-view")
-    book.each do |info|
-      story = Book.new
-      story.title = info.css("h3 a").text.strip
-      story.author = info.css("div.product-shelf-author").text.strip
-      story.price = info.css("a.current.link").text
-      #release_date = book.css("")
-      story.url = info.css("a").attr("href").text
-      end
     end
-
+  end
   def self.clear
     @@all = []
   end
+
 end
 
 
