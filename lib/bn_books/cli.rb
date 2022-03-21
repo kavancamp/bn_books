2e9d5a1c976b282e965eba9fc5350a3c26eca386
Keenah <kvancamp@mail.com>
2022-03-20 23:59:45 -0500

    Update cli.rb

--- lib/bn_books/cli.rb
+++ lib/bn_books/cli.rb
@@ -1,53 +1,60 @@
-class BnBooks::CLI 
+class CLI 
+
     def call
-        puts "            Welcome To BNBooks CLI!!! "
-        display_best_sellers
+        puts "       Welcome To B & N Best Sellers CLI!!! "
+        puts "                  ***********"
+        list_best_sellers
         menu
-        
-      end
+    end
     
-      def display_best_sellers
+    def list_best_sellers
         x = Scraper.new
-        x.scrape_category
-        x.category.each.with_index(1) do |index, category|
-        puts "\n#{category}. #{index}"
+        x.scrape_best_sellers
+        x.title.each_with_index do |title,index|
+        puts "#{index + 1}.#{title}"
+        puts "             "
         end
-      end
-    
-      def menu
+    end
+
+    def menu
         input = nil
         while input != "exit"
-        puts "\nPlease type Book # for more details, or type exit!"
+        puts "***********************************************"
+        puts "Please enter the number of the book you would like more information on, else enter 'list' or exit !"
+        puts "***********************************************"
+        puts "\n"
         input = gets.strip.downcase
-        if (input.to_i > 0) && (input.to_i <= 20)
-          x = Scraper.new
-          x.scrape_category
-          x.scrape_book(x.category[input.to_i-1])
-          Book.display_book_details
-          choose_book
-          break
-        else input == "list"
-            Scraper.clear
-            display_best_sellers
-          end
+           if (input.to_i > 0) && (input.to_i <= 20)
+            x = Scraper.new
+            x.scrape_best_sellers
+            x.scrape_book(x.title[input.to_i-1])
+            Book.display_book_details
+            book_link
+            break
+           elsif input == 'list'
+        list_best_sellers
+    else 
+        puts "\n Invalid Entry, Please try again"
+    end
+end
+end
+    def book_link
+        input = nil
+        while input != "exit"
+        puts "\nEnter book number to go to the web page or 'list' to get categories or 'exit'!"
+        input = gets.strip
+        if (input.to_i > 0) && (input.to_i < 20)
+        system("open https://www.barnesandnoble.com/{Book.all[input.to_i-1].url}")
+        elsif
+            input == "list"
+        Scraper.clear
+            best_sellers
+            menu
+            break
+        else
+            break
         end
-      end
-     
-      def choose_book
-         input = nil
-         while input != "exit"
-         puts "Enter book # to go to the web page or 'list' to get categories or 'exit'!"
-         input = gets.strip
-         if (input.to_i > 0) && (input.to_i < 20)
-           system("open https://www.barnesandnoble.com/#{Book.all[input.to_i-1].url}")
-           elsif
-             input == "list"
-               Scraper.clear
-               display_best_sellers
-               menu
-               break
-            end
-          end
         end
+    end
 
 end
